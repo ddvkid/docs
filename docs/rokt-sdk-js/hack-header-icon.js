cf910cc8975739aaeecdad7152ebcb37aa6cd329
Martin Rubinsztein <martin.rubinsztein@rokt.com>
2019-05-23 17:05:13 +1000

    Definitive final version

--- docs/rokt-sdk-js/hack-header-icon.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  var headerIcon = document.querySelector('header > a');
-  if (headerIcon) {
-    headerIcon.setAttribute('href', 'https://rokt.com');
-  }
-});
